7a6cf44a5871bb00d0eaa37ff9e9e01cdfc1b785
Gasper Katrasnik <gkatrasnik@gmail.com>
2020-11-19 18:00:26 +0100

    fixed consecutive operations

--- script.js
+++ script.js
@@ -16,7 +16,7 @@ function divide(a,b) {
   return parseInt(a/b);  
 }
 
-function operate (numA, numB, operator) {
+function operate(numA, numB, operator) {
     let result;
     if (operator === "add") {
       result = add(numA, numB);
@@ -27,29 +27,29 @@ function operate (numA, numB, operator) {
     } else if (operator === "divide") {
       result = divide(numA, numB);
     }
-    display.textContent = result;
+    return result;
 }
 
 // MAIN CODE
-let firstNum
-let secondNum
-let operator
+let firstNum = 0;
+let secondNum = 0;
+let operator;
 
-
-const calculator = document.querySelector(".container")
+const calculator = document.querySelector(".container");
 const keys = calculator.querySelector(".calculator-keys");
 const display = document.querySelector(".display");
 
 keys.addEventListener("click", e => {
   if (e.target.matches("button")) {
     
-    const key = e.target
-    const action = key.dataset.action   
-    const keyContent = key.textContent
-    const displayedNum = display.textContent
+    const key = e.target;
+    const action = key.dataset.action;   
+    const keyContent = key.textContent;
+    const displayedNum = display.textContent;
 
     // action = NUMBER
-    if (!action) {      
+    if (!action) {
+      
       if (displayedNum === "0") {
         display.textContent = keyContent;
       } else {
@@ -63,29 +63,31 @@ keys.addEventListener("click", e => {
       action === "substract" ||
       action === "multiply" ||
       action === "divide") {
-        firstNum = displayedNum;
-        operator = action;
-        display.textContent = "0";
+        if (firstNum == 0) {
+          firstNum = parseFloat(displayedNum);        
+          operator = action;
+          display.textContent = "0";
+        }else if (firstNum != 0) {
+          secondNum = parseFloat(displayedNum);          
+          firstNum = operate(firstNum, secondNum, operator);       
+          operator = action;
+          display.textContent = "0";
+        }
     }
 
     // action = CLEAR
     if ( action === "clear") {
-      display.textContent = "0"
+      display.textContent = "0";
     }
 
-    // action = OPERATE
+    // action = CALCULATE
     if (action === "calculate") {
-      secondNum = displayedNum;
-      operate(firstNum, secondNum, operator)
-
+      secondNum = parseInt(displayedNum);
+      display.textContent = operate(firstNum, secondNum, operator);
+      firstNum = 0;
+      secondNum = 0;
     }
 
-    
-
-
-
-
-    
   }
 });
 
